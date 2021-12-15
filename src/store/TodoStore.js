c0795131ccfacc24b2e465b5051562996dddcbc2
lkveless <ard01@mail.ru>
2021-12-15 17:31:12 +0300

    rework stores

---
+++ src/store/TodoStore.js
@@ -0,0 +1,36 @@
+import { makeAutoObservable } from "mobx"
+
+export default class TodoStore{
+    todos = []
+    constructor() { 
+        makeAutoObservable(this)
+    }
+
+    addTodos(todos) {
+        this.todos = todos.todos.slice()
+    }
+
+    
+    
+    todoIsDone(id) {
+        this.todos = this.todos.map(todo => todo.id === id ? {...todo, isCompleted:!todo.isCompleted}: todo)
+    }
+
+    todoIsFavourite(id) {
+        this.todos = this.todos.map(todo => todo.id === id ? {...todo, isFavourite:!todo.isFavourite}: todo)
+    }
+
+    todoSort(sort){
+        console.log('sort', sort)
+        if (sort.includes('done')){
+            this.todos = this.todos.filter(todo => todo.isCompleted === true)
+        }
+        if (sort.includes('favourite')){
+            this.todos = this.todos.filter(todo => todo.isFavourite === true)
+        }
+        if (sort.includes('not done')){
+            this.todos = this.todos.filter(todo => todo.isCompleted === false)
+        }
+        
+    }
+}
--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -4,6 +4,32 @@ export default class UserStore{
     constructor(){
         this._isAuth = false
         this._user={}
+        this._userData=[]
         makeAutoObservable(this)
+    }
+
+    setIsAuth(bool){
+        this._isAuth = bool
+    }
+
+    setUser(user){
+        this._user = user
+    }
+
+    setUserData(userData){
+        this._userData = userData
+    }
+
+
+    get isAuth(){
+        return this._isAuth
+    }
+
+    get user(){
+        return this._user
+    }
+
+    get userData(){
+        return this._userData
     }
 }
