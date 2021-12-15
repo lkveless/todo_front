6dfa38f2c2a126a4620d403c3b0c8541e77d321a
lkveless <ard01@mail.ru>
2021-12-15 17:30:42 +0300

    add fetch data from backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import React, {useContext, useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter} from 'react-router-dom'
+import {Context} from "./index"
+import NavBar from './components/Navbar'
 import NavRouter from './components/NavRouter'
+import './App.css';
+import { observer } from 'mobx-react-lite';
+import {Spinner} from "react-bootstrap";
+import { check } from './http/userAPI';
 
 
-const App = () => {
+const App = observer(() => {
+  const {user} = useContext(Context)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+      check().then(data => {
+          user.setUser(true)
+          user.setIsAuth(true)
+      }).finally(() => setLoading(false))
+  }, [user])
+
+  if (loading) {
+      return <Spinner animation={"grow"}/>
+  }
+
   return (
     <BrowserRouter>
+      <NavBar />
       <NavRouter />
     </BrowserRouter>
   )
-}
+})
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import UserStore from './store/UserStore';
+import TodoStore from './store/TodoStore';
+
+export const Context = createContext(null)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Context.Provider value={{
+    user: new UserStore(),
+    todoList: new TodoStore()
+  }}>
     <App />
-  </React.StrictMode>,
+  </Context.Provider>,
   document.getElementById('root')
 );
 
